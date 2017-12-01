131f0d69f586a17ec75fdbdf4bdd9c741536918f
Chris Bolin <bolin.chris@gmail.com>
2017-11-30 23:52:28 -0700

    prettier src/

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React, { Component, isValidElement, Children, createElement } from "react";
+import React, {
+  Component,
+  isValidElement,
+  Children,
+  createElement
+} from "react";
 
 // base class that detects offline/online changes
 class Base extends Component {
   constructor() {
     super();
     this.state = {
-      online: navigator.onLine,
+      online: navigator.onLine
     };
     // bind event handlers
     this.goOnline = this.goOnline.bind(this);
@@ -16,46 +21,50 @@ class Base extends Component {
     let { wrapperType } = this.props;
 
     // usual case: one child that is a react Element
-    if (React.isValidElement(children)) { return children; }
+    if (React.isValidElement(children)) {
+      return children;
+    }
 
     // no children
-    if (!children) { return null; }
+    if (!children) {
+      return null;
+    }
 
     // string children, multiple children, or something else
     const childrenArray = Children.toArray(children);
     const firstChild = childrenArray[0];
     // use wrapperType if specified
     if (!wrapperType) {
-      if (typeof firstChild === 'string' || firstChild.type === 'span') {
+      if (typeof firstChild === "string" || firstChild.type === "span") {
         // use span for string or span children
-        wrapperType = 'span';
+        wrapperType = "span";
       } else {
         // fallback on div
-        wrapperType = 'div';
+        wrapperType = "div";
       }
     }
     return createElement(wrapperType, {}, ...childrenArray);
   }
   goOnline() {
-		this.callOnChangeHandler(true);
+    this.callOnChangeHandler(true);
     this.setState({ online: true });
   }
   goOffline() {
-		this.callOnChangeHandler(false);
+    this.callOnChangeHandler(false);
     this.setState({ online: false });
   }
-	callOnChangeHandler(online) {
-		if (this.props.onChange) {
-			this.props.onChange(online);
-		}
-	}
+  callOnChangeHandler(online) {
+    if (this.props.onChange) {
+      this.props.onChange(online);
+    }
+  }
   componentDidMount() {
-    window.addEventListener('online', this.goOnline);
-    window.addEventListener('offline', this.goOffline);
+    window.addEventListener("online", this.goOnline);
+    window.addEventListener("offline", this.goOffline);
   }
   componentWillUnmount() {
-    window.removeEventListener('online', this.goOnline);
-    window.removeEventListener('offline', this.goOffline);
+    window.removeEventListener("online", this.goOnline);
+    window.removeEventListener("offline", this.goOffline);
   }
 }
 
