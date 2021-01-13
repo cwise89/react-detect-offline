1e5fdd9b6f600122f89a3a79afa550644567e591
Cody Wise <clwise1989@gmail.com>
2021-01-13 09:57:11 -0700

    started ts project
    started TS
    
    added git ignore
    
    clean up
    
    removed node_modules and ignored

---
+++ demo/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+	return <div>Hello World</div>;
+}
+
+export default App;
---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+export declare type ICounterProps = {
+    className?: string;
+};
+declare const App: React.FC<ICounterProps>;
+export default App;
