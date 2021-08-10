4566dec4fd28e0ea6a4dd8d98aaa689a3322cedb
Cody Wise <cwise@fluentstream.com>
2021-08-10 09:05:16 -0500

    just a checkpoint for the weekend

--- demo/src/ComponentOne.tsx
+++ demo/src/ComponentOne.tsx
@@ -4,7 +4,8 @@ export function One() {
 	useOnlineEffect(online => console.log('fired One', online), {
         enabled: true,
 		timeout: 5000,
-		interval: 15000,
+		interval: 5000,
+        url: "google.com"
 	});
 	return <div>Hello One</div>;
 }
