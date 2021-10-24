74f81165bd4daf3db03db17fc19fc6b5509f0ebc
Taanileka Maama <taani1maama@live.com>
2021-10-24 15:41:55 -0700

    Tm v3 ping config (#111)
    * Created hashed object that we can store to the window to make sure that duplicates are not being ran.
    
    * Added the unmount delete from the window.

--- dist/index.js
+++ dist/index.js
@@ -40,12 +40,42 @@ function __rest(s, e) {
     return t;
 }
 
-function __spreadArrays() {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
+function __awaiter(thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+}
+
+function __generator(thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
 }
 
 var needsPolling = function (navigator) {
@@ -110,42 +140,41 @@ var useOnlineEffect = function (callback, pollingOptions) {
     var mustPoll = needsPolling(navigator);
     var _a = getPollingConfigs(pollingOptions, mustPoll), enabled = _a.enabled, pingConfig = __rest(_a, ["enabled"]);
     react.useEffect(function () {
-        var _a, _b;
         // initial online event fired.
         callback(true);
         window.addEventListener('online', goOnline);
         window.addEventListener('offline', goOffline);
+        pingConfig['callback'] = callback;
         // initialize setInterval id so we can clean up on unmount.
         var intervalId;
-        if ((mustPoll || enabled) && 'url' in pingConfig) {
-            if (!((_a = window._useOnlineEffect_) === null || _a === void 0 ? void 0 : _a.pingerExist)) {
-                window._useOnlineEffect_ = {
-                    pingerExist: true,
-                    callbackList: [callback]
-                };
-            }
-            else {
-                window._useOnlineEffect_.callbackList = __spreadArrays([callback], (_b = window._useOnlineEffect_) === null || _b === void 0 ? void 0 : _b.callbackList);
-            }
-            var url_1 = pingConfig.url, timeout_1 = pingConfig.timeout, interval = pingConfig.interval;
-            intervalId = setInterval(function () {
-                ping({
-                    url: url_1,
-                    timeout: timeout_1,
-                }).then(function () {
-                    var _a, _b;
-                    console.log("CALLBACKS: ", (_a = window._useOnlineEffect_) === null || _a === void 0 ? void 0 : _a.callbackList);
-                    (_b = window._useOnlineEffect_) === null || _b === void 0 ? void 0 : _b.callbackList.forEach(function (cb) {
-                        cb(true);
-                    });
-                }).catch(function () {
-                    var _a, _b;
-                    console.log("CALLBACKS: ", (_a = window._useOnlineEffect_) === null || _a === void 0 ? void 0 : _a.callbackList);
-                    (_b = window._useOnlineEffect_) === null || _b === void 0 ? void 0 : _b.callbackList.forEach(function (cb) {
-                        cb(false);
-                    });
+        // hash config for key to store our actual config.
+        var hashedConfig = JSON.stringify(__assign(__assign({}, pingConfig), { callback: pingConfig["callback"] + "" }));
+        if ((mustPoll || enabled) && "url" in pingConfig && !window[hashedConfig]) {
+            window[hashedConfig] = __assign({}, pingConfig);
+            var _a = window[hashedConfig], url_1 = _a.url, timeout_1 = _a.timeout, interval = _a.interval, callback_1 = _a.callback;
+            intervalId = setInterval(function () { return __awaiter(void 0, void 0, void 0, function () {
+                return __generator(this, function (_a) {
+                    switch (_a.label) {
+                        case 0:
+                            _a.trys.push([0, 2, , 3]);
+                            return [4 /*yield*/, ping({
+                                    url: url_1,
+                                    timeout: timeout_1,
+                                })];
+                        case 1:
+                            _a.sent();
+                            console.log("CALLBACK: ", callback_1);
+                            callback_1(true);
+                            return [3 /*break*/, 3];
+                        case 2:
+                            _a.sent();
+                            console.log("CALLBACK: ", callback_1);
+                            callback_1(false);
+                            return [3 /*break*/, 3];
+                        case 3: return [2 /*return*/];
+                    }
                 });
-            }, interval);
+            }); }, interval);
         }
         return function () {
             window.removeEventListener('online', goOnline);
